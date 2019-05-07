297561f24c91c03fb35ab5624b34ac999ea2ea67
shiyanfei <yanfei1819@163.com>
2019-05-07 15:17:28 +0800

    初始化未提交代码

---
+++ mybatis-mapper-demo/src/main/java/com/yanfei1819/mybatismapperdemo/MybatisMapperDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yanfei1819.mybatismapperdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisMapperDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisMapperDemoApplication.class, args);
+    }
+
+}
