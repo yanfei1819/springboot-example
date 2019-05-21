3310ba0103a4df4ec320f12b434890b0bfbb6cba
shiyanfei <yanfei1819@163.com>
2019-05-21 14:32:20 +0800

    jpa

--- jpa-demo/src/main/java/com/yanfei1819/jpademo/repository/UserRepository.java
+++ jpa-demo/src/main/java/com/yanfei1819/jpademo/repository/UserRepository.java
@@ -2,9 +2,16 @@ package com.yanfei1819.jpademo.repository;
 
 import com.yanfei1819.jpademo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by 追梦1819 on 2019-05-05.
  */
 public interface UserRepository extends JpaRepository<User,Long> {
+    User findByName(String name);
+
+    @Query("select u from User u where u.id = :id ")
+    // @Query("select u from User u where u.id = ?1 ")
+    User withIdQuery(@Param("id") Long id);
 }
--- jpa-demo/src/main/java/com/yanfei1819/jpademo/web/controller/UserController.java
+++ jpa-demo/src/main/java/com/yanfei1819/jpademo/web/controller/UserController.java
@@ -5,6 +5,7 @@ import com.yanfei1819.jpademo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -21,6 +22,21 @@ public class UserController {
     @ResponseBody
     @GetMapping("/queryUser")
     public List<User> queryUser(){
+        // 通过 jpa 内置的方法进行查询
         return userRepository.findAll();
+    }
+
+    @ResponseBody
+    @GetMapping("/queryUser/{name}")
+    public User queryUserByName(@PathVariable String name){
+        // 通过自定义的方法进行查询
+        return userRepository.findByName(name);
+    }
+
+    @ResponseBody
+    @GetMapping("/queryUser2/{id}")
+    public User queryUserById(@PathVariable Long id){
+        // 通过 @Query 注解查询
+        return userRepository.withIdQuery(id);
     }
 }
