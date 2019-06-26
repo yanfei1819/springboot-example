09829b3141540ff33b4395e1ceac8387e790ab13
shiyanfei <yanfei1819@163.com>
2019-06-26 18:00:21 +0800

    activemq的整合

---
+++ activemq/src/test/java/com/yanfei1819/activemq/ActivemqApplicationTests.java
@@ -0,0 +1,18 @@
+package com.yanfei1819.activemq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActivemqApplicationTests {
+
+
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
