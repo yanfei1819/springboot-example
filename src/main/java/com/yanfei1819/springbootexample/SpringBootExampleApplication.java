e91893b78aadb37545be1ca428a7efde12ef7617
yanfei1819 <yanfei1819@163.com>
2019-12-27 16:57:43 +0800

    rename

--- src/main/java/com/yanfei1819/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/com/yanfei1819/springbootexample/SpringBootExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootExampleApplication {
+public class SpringBootExampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootExampleApplication.class, args);
+        SpringApplication.run(SpringBootExampleApplication.class, args);
     }
 
 }
